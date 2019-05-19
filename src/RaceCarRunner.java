cb361ada0bafcf6a43106d540fb2c0611e606f25
league <league@imac-56.attlocal.net>
2019-05-18 17:30:33 -0700

    platypus

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,17 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	 RaceCar bob = new RaceCar("toyota",5 );
+			 
 		// 2. Print the RaceCar's position in the race
-
+          System.out.println(bob.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		bob.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(bob.isDamaged()) {
+	bob.pit();
+}
 		// 5. Help the car move into first place.
-
+          bob.overtake();
 	}
 }
